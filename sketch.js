d7ccb55ca52c0a2be78260e74434ab62c791d350
beaforesti <bea_fiore@live.it>
2019-10-08 11:24:19 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var allMyCereals = []; //array
 var cerealsNumber = 40;
+var eatenCereals = 0;
 
 function preload() {
   // put preload code here
@@ -9,11 +10,12 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
 
+
   //create cereals
   for (var i = 0; i < cerealsNumber; i++) {
     //create instance
-    var tempX = random(windowWidth - windowWidth / 4, windowWidth / 4);
-    var tempY = random(windowHeight - windowHeight / 4, windowHeight / 4);
+    var tempX = random(windowWidth - windowWidth / 3, windowWidth / 3);
+    var tempY = random(windowHeight - windowHeight / 3, windowHeight / 3);
     var tempR = 50;
 
     var tempCereal = new Cereal(tempX, tempY, tempR);
@@ -25,14 +27,7 @@ function setup() {
 
 function draw() {
   background('coral');
-  //text
-  push()
 
-  fill(0);
-  textSize(20);
-  text('Click on the cereals to eat them', 20, 40);
-
-  pop();
 
   var xBowl = windowWidth / 2;
   var yBowl = windowHeight / 2;
@@ -51,7 +46,6 @@ function draw() {
   ellipse(xBowl, yBowl, diBowl - diBowl / 15);
 
 
-
   // spoon
   push()
   fill(80);
@@ -61,6 +55,8 @@ function draw() {
   ellipse(mouseX, mouseY, 80, 50);
   pop();
 
+
+
   // draw cereals
   for (var i = 0; i < allMyCereals.length; i++) {
     var tempCereal = allMyCereals[i];
@@ -68,7 +64,26 @@ function draw() {
     tempCereal.display();
   }
 
+  //text
+  push()
+
+  fill(0);
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text('Click on the cereals to eat them!', windowWidth / 2, windowHeight / 2);
+pop();
+
+  if (eatenCereals == 40) {
+
+    background('gold');
+    fill(240);
+    textSize(100);
+    textAlign(CENTER, CENTER);
+    text('yummy!', windowWidth / 2, windowHeight / 2);
 }
+
+}
+
 // eat cereals on click
 function mouseClicked() {
   for (var i = 0; i < allMyCereals.length; i++) {
@@ -92,6 +107,7 @@ function Cereal(_x, _y, _diameter) {
     var d = dist(mouseX, mouseY, this.x, this.y)
     if (d < this.size) {
       this.size = 0;
+      eatenCereals++;
     }
   }
 
@@ -101,6 +117,8 @@ function Cereal(_x, _y, _diameter) {
     frameRate(15);
     this.x = this.x + random(-10, 10);
     this.y = this.y + random(-10, 10);
+    constrain(this.x, (windowWidth - 100) / 2, (windowWidth + 100) / 2);
+    constrain(this.y, (windowHeight - 100) / 2, (windowHeight + 100) / 2);
     pop()
   }
 
